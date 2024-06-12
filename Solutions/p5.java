af4c570b1d0852847181cd22580be0e5658496da
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-06-12 21:16:06 +0600

    solved the problem 5 - difference of sum square and square sum

---
+++ Solutions/p5.java
@@ -0,0 +1,7 @@
+public class p5 {
+    public static void main(String[] args){
+        int n = 100;
+        int diff_of_sumSqr_sqrSum = n*(n*n - 1)*(3*n + 2)/12;
+        System.out.println(diff_of_sumSqr_sqrSum);
+    }
+}
