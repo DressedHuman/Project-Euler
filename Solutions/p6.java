bf8771659d943ee748164e7eb488707e66929917
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-06-12 21:26:41 +0600

    solved the problem 6 - 10001st prime

---
+++ Solutions/p6.java
@@ -0,0 +1,30 @@
+public class p6 {
+    public static boolean isPrime(Integer num){
+        for(int i=2; i<(Math.sqrt(num)+1); ++i){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int nextPrime(Integer currentPrime){
+        currentPrime += 2;
+        while(!isPrime(currentPrime)){
+            currentPrime += 2;
+        }
+        return currentPrime;
+    }
+
+    public static void main(String[] args){
+        int count = 2; // assume we found 2, 3 as the first 2 primes; so current prime is 3;
+        int currentPrime = 3;
+
+        while(count<10001){
+            currentPrime = nextPrime(currentPrime);
+            ++count;
+        }
+
+        System.out.println(currentPrime);
+    }
+}
