53ac700b0818f87b8769136aaae1ece4440e581d
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-06-13 18:45:39 +0600

    solved the problem 5 - smallest number divided by 1 to 20; Correction: previous p5 and p6 were actually p6 and p7 respectively

--- Solutions/p5.java
+++ Solutions/p5.java
@@ -1,7 +1,27 @@
 public class p5 {
+    public static long gcd(long num1, long num2){
+        if(num1%num2==0){
+            return num2;
+        }
+        return gcd(num2, num1%num2);
+    }
+
+    public static long lcm(long num1, long num2){
+        return (num1*num2)/gcd(num1, num2);
+    }
+
+    public static long LCM(int[] nums){
+        long current_lcm = nums[0];
+        for(int i=1; i<nums.length; ++i){
+            current_lcm = lcm(current_lcm, nums[i]);
+        }
+
+        return current_lcm;
+    }
+
     public static void main(String[] args){
-        int n = 100;
-        int diff_of_sumSqr_sqrSum = n*(n*n - 1)*(3*n + 2)/12;
-        System.out.println(diff_of_sumSqr_sqrSum);
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        long lcm_of_nums = LCM(arr);
+        System.out.println(lcm_of_nums);
     }
 }
--- Solutions/p6.java
+++ Solutions/p6.java
@@ -1,30 +1,7 @@
 public class p6 {
-    public static boolean isPrime(Integer num){
-        for(int i=2; i<(Math.sqrt(num)+1); ++i){
-            if(num%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static int nextPrime(Integer currentPrime){
-        currentPrime += 2;
-        while(!isPrime(currentPrime)){
-            currentPrime += 2;
-        }
-        return currentPrime;
-    }
-
     public static void main(String[] args){
-        int count = 2; // assume we found 2, 3 as the first 2 primes; so current prime is 3;
-        int currentPrime = 3;
-
-        while(count<10001){
-            currentPrime = nextPrime(currentPrime);
-            ++count;
-        }
-
-        System.out.println(currentPrime);
+        int n = 100;
+        int diff_of_sumSqr_sqrSum = n*(n*n - 1)*(3*n + 2)/12;
+        System.out.println(diff_of_sumSqr_sqrSum);
     }
 }
