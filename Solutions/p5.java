87c8dec097e6e81574d284a27c45827e2b65e23c
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-06-14 14:19:30 +0600

    solved the problem 5 - Smallest number divisible by 1 to 20, with a different but more efficient approach

--- Solutions/p5.java
+++ Solutions/p5.java
@@ -1,4 +1,4 @@
-public class p5 {
+/* public class p5 {
     public static long gcd(long num1, long num2){
         if(num1%num2==0){
             return num2;
@@ -25,3 +25,43 @@ public class p5 {
         System.out.println(lcm_of_nums);
     }
 }
+ */
+
+import java.util.Scanner;
+
+public class p5 {
+    public static boolean isPrime(int num){
+        for(int i=2; i<=Math.sqrt(num); ++i){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int nextPrime(int current_prime){
+        current_prime += 2;
+        while(!isPrime(current_prime)){
+            current_prime += 2;
+        }
+        return current_prime;
+    }
+
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        // assume that we already found the first prime, 2
+        System.out.printf("Type in the last digit: ");
+        int last_digit = scanner.nextInt(); // 20
+        long lcm = (long) Math.pow(2, (int) (Math.log(last_digit)/Math.log(2)));
+        int current_prime = 3;
+
+        while(current_prime<=last_digit){
+            lcm *= (long) Math.pow(current_prime, (int) (Math.log(last_digit)/Math.log(current_prime)));
+            current_prime = nextPrime(current_prime);
+        }
+        
+        System.out.println(lcm);
+        
+    }
+ }
