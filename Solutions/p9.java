5cc5d24e444e84e05fa9258ae20dd91cb5e77b38
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-06-20 23:04:44 +0600

    solved the problem 8 - largest pythagorean numbers where a<b<c, a+b+c=1000 and a*a+b*b=c*c

---
+++ Solutions/p9.java
@@ -0,0 +1,27 @@
+public class p9 {
+    public static boolean check(int a, int b, int c) {
+        if (a * a + b * b == c * c) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        boolean found = false;
+        int a = 1;
+
+        while (!found && 3 * (a + 1) <= 1000) {
+            int b = a + 1;
+            int limit = (1000 - a) / 2;
+            while (b <= limit) {
+                int c = (1000 - a - b);
+                if (c * c == a * a + b * b) {
+                    System.out.printf("%d, %d, %d", a, b, c);
+                    found = true;
+                }
+                ++b;
+            }
+            ++a;
+        }
+    }
+}
